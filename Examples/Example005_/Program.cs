7eaea8d444aabd6e3a6eca063fc97ea86a5b5783
dilshodf <144156304+dilshodf@users.noreply.github.com>
2023-09-24 10:27:39 +0500

    add Example 005 Condition If-Else

---
+++ Examples/Example005_/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Write username ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "dilshod")
+{
+    System.Console.WriteLine("Hurray it's Dilshod");
+}
+
+else
+{
+    System.Console.Write("Hello, ");
+    System.Console.WriteLine(username);
+}
+
