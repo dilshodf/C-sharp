925f9ac33e68dce2897731dfdb86f9dcf275f6e7
dilshodf <144156304+dilshodf@users.noreply.github.com>
2023-09-24 13:13:43 +0500

    add Задача 8

--- Seminars/Homework1/Program.cs
+++ Seminars/Homework1/Program.cs
@@ -49,3 +49,26 @@
 // {
 //     System.Console.WriteLine($"{num} -> no");
 // }
+
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// System.Console.Write("Input N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int M = 1;
+
+// System.Console.Write($"{N} -> ");
+
+// while (M <= N)
+// {
+//     if (M % 2 == 0)
+//     {
+//         System.Console.Write($"{M++}, ");
+//     }
+//     else
+//     {
+//         M++;
+//     }
+// }
