b62a66b8df72549ba4ee89bd430a2bfb4e2677c9
dilshodf <144156304+dilshodf@users.noreply.github.com>
2023-09-24 12:55:46 +0500

    add Задача 6

--- Seminars/Homework1/Program.cs
+++ Seminars/Homework1/Program.cs
@@ -32,3 +32,20 @@
 // if (c > max) max = c;
 
 // System.Console.WriteLine($"{a}, {b}, {c} -> {max}");
+
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// System.Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+// {
+//     System.Console.WriteLine($"{num} -> yes");
+// }
+// else
+// {
+//     System.Console.WriteLine($"{num} -> no");
+// }
