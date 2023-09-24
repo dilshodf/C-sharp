5081d44cb474b530335d7d6585b493d8d7ce9279
dilshodf <144156304+dilshodf@users.noreply.github.com>
2023-09-24 19:49:25 +0500

    add ArrayMethod

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 31, 12, 23, 45, 15, 26, 17, 15 };
+
+int n = array.Length;
+int find = 5;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;                          // если в массиве несколько одинаковых значений, останавливаем при первом же
+    }
+    index++;
+}
