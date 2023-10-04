e52c4aa6e26f7ad09b32731cdda18cce9e89e6e2
dilshodf <144156304+dilshodf@users.noreply.github.com>
2023-10-04 16:10:32 +0500

    add Exercise 25

---
+++ Seminars/Homework4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// int PowerOf(int A, int B)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= B; i++)
+//     {
+//         result *= A;
+//     }
+//     return result;
+// }
+
+
+// System.Console.Write("Input A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine($"{A}, {B} -> {PowerOf(A, B)}");
