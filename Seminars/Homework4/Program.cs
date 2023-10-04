fc70894aaaa349e5dad3c41f91d41c7092140287
dilshodf <144156304+dilshodf@users.noreply.github.com>
2023-10-04 16:21:17 +0500

    add Exercise 27

--- Seminars/Homework4/Program.cs
+++ Seminars/Homework4/Program.cs
@@ -19,3 +19,27 @@
 // int B = Convert.ToInt32(Console.ReadLine());
 
 // System.Console.WriteLine($"{A}, {B} -> {PowerOf(A, B)}");
+
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int DigitCount(int num)
+// {
+//     int result = 0;
+//     for (int i = 0; num > 0; i++)
+//     {
+//         result += num % 10;
+//         num /= 10;
+//     }
+//     return result;
+// }
+
+// System.Console.Write("Input num: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"{num} -> {DigitCount(num)}");
