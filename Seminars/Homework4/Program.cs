a2ccabccbda91c845b7a6af630b6b6b33e967883
dilshodf <144156304+dilshodf@users.noreply.github.com>
2023-10-04 16:47:30 +0500

    add Exercise 29

--- Seminars/Homework4/Program.cs
+++ Seminars/Homework4/Program.cs
@@ -43,3 +43,39 @@
 // System.Console.Write("Input num: ");
 // int num = Convert.ToInt32(Console.ReadLine());
 // System.Console.WriteLine($"{num} -> {DigitCount(num)}");
+
+
+
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] EightElementsArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+// }
+
+// System.Console.Write("Input size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input min value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input max value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// PrintArray(EightElementsArray(size, min, max));
